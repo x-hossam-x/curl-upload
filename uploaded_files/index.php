b78dee5cdad263e71063a2f8a49ca7e98d99bccc
Hossam Joseph <hossam.joseph@gmail.com>
2013-03-24 23:37:21 +0200

    Create index.php

---
+++ uploaded_files/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml">
+ <head>
+  <title>Do not have permission to enter here</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ </head>
+ <body>
+	<p>Do not have permission to enter here</p>
+ </body>
+</html>
