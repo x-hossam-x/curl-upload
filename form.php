11ee4ac2f8c2ba5fca15c715d6ef40bf5fdb7110
Hossam Joseph <hossam.joseph@gmail.com>
2013-03-24 23:34:19 +0200

    form

---
+++ form.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml">
+ <head>
+  <title>Upload Image File To Remote Server With PHP CURL</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ </head>
+ <body>
+	<form action="uploader.php" method="post" enctype="multipart/form-data">
+	  <input type="file" name="my_file" />
+	  <input type="submit" name="upload" value="Upload" />
+	</form>
+ </body>
+</html>
